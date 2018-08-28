d382f76e09142d403b1f3b3a6340aabebb7611ca
Michał Makosiewicz <makosiewicz2@gmail.com>
2018-08-28 23:13:25 +0200

    ThreeDecimals

--- BarkingDog/src/barkingDog.java
+++ BarkingDog/src/barkingDog.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
-
 public class barkingDog {
+    public static void main(String[] args) {
+
+        System.out.println(bark(true,9));
+        System.out.println(bark(true,42));
+        System.out.println(bark(true,23));
+
+
+    }
+
+    public static boolean bark(boolean bark,int hour){
+        if (((hour>=0)&&((hour<8)) || (hour<=24)&&(hour>22)) && (bark)) {
+            return true;
+        }
+
+        return false;
+
+    }
 }
