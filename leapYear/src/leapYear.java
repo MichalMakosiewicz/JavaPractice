5ca7b42d94a1708e1d310b56e33b9ed75b9a7354
Michał Makosiewicz <makosiewicz2@gmail.com>
2018-08-28 22:29:51 +0200

    leapYear

--- leapYear/src/leapYear.java
+++ leapYear/src/leapYear.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
-
 public class leapYear {
+
+    public static void main(String[] args) {
+        System.out.println(isLeapYear(1234));
+        System.out.println(isLeapYear(2000));
+        System.out.println(isLeapYear(-1234));
+        System.out.println(isLeapYear(12345678));
+        System.out.println(isLeapYear(1800));
+
+
+    }
+
+    public static boolean isLeapYear(int year){
+
+        if ((year>=1)&&(year<=9999)){
+            if(((year%4==0 && year%100!=0) || (year%400==0))){
+                return true;
+            }
+        }
+        return false;
+    }
 }
